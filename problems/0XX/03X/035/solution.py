30bb14c268d67d629aa1b295669f8287ff7127ba
jamsidedown <46314954+jamsidedown@users.noreply.github.com>
2021-01-01 16:22:38 +0000

    Refactored is_prime in p35

--- problems/0XX/03X/035/solution.py
+++ problems/0XX/03X/035/solution.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def run() -> int:
     return sum(
         all(is_prime(int(num[i:] + num[0:i])) for i in range(len(num)))
@@ -6,14 +9,9 @@ def run() -> int:
 
 
 def is_prime(n: int) -> bool:
-    if n < 2:
-        return False
-    i = 2
-    while i * i <= n:
-        if n % i == 0:
-            return False
-        i += 1 + (i > 2)
-    return True
+    if n < 3:
+        return n == 2
+    return n % 2 and all(n % i > 0 for i in range(3, int(sqrt(n)) + 1, 2))
 
 
 if __name__ == '__main__':
